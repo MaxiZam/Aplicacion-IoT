3d202b94d1964d24d0d757e97fa46301ab5ae177
Maximiliano Zambrana <maxizambrana14@gmail.com>
2024-08-19 11:57:32 -0300

    pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'web-iot-app';
 }
+
