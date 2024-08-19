2a4d5b29acf546967412a52f3da473d7c562f05f
Maximiliano Zambrana <maxizambrana14@gmail.com>
2024-08-19 12:15:44 -0300

    inicio de div de semaforo UV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeaderComponent } from "./header/header.component";
+import { TrafficLightComponent } from './traffic-light/traffic-light.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, HeaderComponent, TrafficLightComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
-export class AppComponent {
-  title = 'web-iot-app';
+export class AppComponent implements OnInit {
+  trafficLightValue: string = '';
+
+  ngOnInit(): void {
+    this.fetchTrafficLightValue();
+  }
+
+  fetchTrafficLightValue(): void {
+    // Simula la obtención del valor desde la base de datos
+    this.trafficLightValue = '3'; // Este valor se obtendría de la base de datos
+  }
 }
 
